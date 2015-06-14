83cf0d8110a62c18c324e71f63f8e7004ce0b256
Daniel de Aguiar <daniel.bdaguiar@gmail.com>
2015-06-14 15:36:39 -0300

    Create RoutesListActivity

---
+++ app/src/androidTest/java/at_challenge/deaguiar/daniel/atchalleng/ApplicationTest.java
@@ -0,0 +1,13 @@
+package at_challenge.deaguiar.daniel.atchalleng;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
